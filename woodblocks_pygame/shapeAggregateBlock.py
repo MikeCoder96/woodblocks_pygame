c81d12b21b30b11d15455e327ac8df305af448c8
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-04-17 11:11:09 +0200

    Implemented setters + comments.

--- woodblocks_pygame/shapeAggregateBlock.py
+++ woodblocks_pygame/shapeAggregateBlock.py
@@ -4,12 +4,23 @@ class ShapeAggregateBlock():
 		self.componentY = int(componentY / blockSize)
 
 	def __init__(self, rawBlocks: list, blockSize: int) -> None:
-		print(rawBlocks)
 		self.componentX = int(rawBlocks[0] / blockSize)
 		self.componentY = int(rawBlocks[1] / blockSize)
 
 	def get_componentX(self) -> int:
+		"""
+		Returns x-position of the single block, blockSize-agnostic.
+		"""
 		return self.componentX
 
 	def get_componentY(self) -> int:
+		"""
+		Returns y-position of the single block, blockSize-agnostic.
+		"""
 		return self.componentY
+
+	def set_componentX(self, x: int):
+		self.componentX = x
+
+	def set_componentY(self, y: int):
+		self.componentY = y
